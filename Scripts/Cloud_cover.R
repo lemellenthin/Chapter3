5abafb6e752139ae9705c20168fa645995ac8561
Lauren Mellenthin <laurmel@iastate.edu>
2019-06-03 14:19:15 -0500

    Add files via upload

--- Scripts/Cloud_cover.R
+++ Scripts/Cloud_cover.R
@@ -2,17 +2,51 @@
 #### cloud cover new dataset ###########
 ########################################
 
-# THIS IS CLIPPING THE CLOUD COVER DATASET TO ANALYZE THE SIGNIFICANCE ACROSS POLYS WITH CSV
-# ALSO THIS PREPS IT FOR THE MAXENT ANALYSIS
-
 # from website
-# https://visibleearth.nasa.gov/view.php?id=85843
+# https://www.ipcc-data.org/observ/clim/cru_ts2_1.html
 
 # load packages
 library(raster)
 library(rgdal)
 library(rgeos)
 library(ggplot2)
+
+
+cloud.raw <- stack(list.files('./Analysis_Scripts/Chapter3/Climate Data/Cloud_cover/cru_cld_clim_1991-2000', full.names = T, 
+                              pattern = '.tif'))
+cloud.raw
+range(values(cloud.raw))
+
+rescale <- function(x, x.min = NULL, x.max = NULL, new.min = 0, new.max = 1) {
+  if(is.null(x.min)) x.min = min(x)
+  if(is.null(x.max)) x.max = max(x)
+  new.min + (x - x.min) * ((new.max - new.min) / (x.max - x.min))
+}
+
+right <- rescale(cloud.raw, x.min = 0, x.max = 255, new.min = 0, new.max = 1)
+right2 <- mean(right)
+right2
+
+writeRaster(right2, paste0('Analysis_Scripts/Chapter3/Climate Data/Cloud_cover/IPCC_data'),format = 'GTiff')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # NASA <- raster("./Analysis_Scripts/Chapter3/Climate Data/globalcldfr_amo_200207-201504_geo.tif")
 # identicalCRS(NASA, Polygons)
@@ -25,7 +59,14 @@ library(ggplot2)
 # writeRaster(NASA1, filename="./Analysis_Scripts/Chapter3/Climate Data/Cropped_Cloud.tif")
 
 NASA <- raster("./Analysis_Scripts/Chapter3/Climate Data/Cropped_Cloud.tif")
+
+# make note of the units of each var somewhere
+# edges, bounces, no data how do you check this and play around with what you want
+# look at the different cut off points
+# make midquart as the midpoint which can pull it apart enough
+
 AllPolysforanalysis <- readOGR("./Analysis_Scripts/Chapter3/Shapefiles/AllPolysforAnalysis/chull.shp")
+
 Records <- matrix(NA, nrow = 311, ncol = 7)
 colnames(Records) <- c("binomial","CloudMin","Cloud1Q","CloudMed","CloudMea","Cloud3Q","CloudMax")
 
@@ -43,6 +84,7 @@ RecordsDF[] <- lapply(RecordsDF, gsub, pattern="\\Max.   :", replacement="")
 RecordsDF[] <- lapply(RecordsDF, gsub, pattern="\\Mean   :", replacement="")
 RecordsDF[] <- lapply(RecordsDF, gsub, pattern="\\Median :", replacement="")
 RecordsDF[] <- lapply(RecordsDF, gsub, pattern="\\Min.   :", replacement="")
+
 RecordsDF[] <- lapply(RecordsDF, gsub, pattern="1st Qu.:", replacement="")
 RecordsDF[] <- lapply(RecordsDF, gsub, pattern="3rd Qu.:", replacement="")
 RecordsDF
